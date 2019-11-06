0c994f9d2a073f5ba7e308e200a00941e231eb7f
Irene Vios <35557223+irevios@users.noreply.github.com>
2019-11-06 08:42:12 +0100

    Inicio

---
+++ objetos.js
@@ -0,0 +1,37 @@
+"use strict";
+class Cliente {
+	constructor(){
+
+	}
+}
+class Venta {
+	constructor(){
+		
+	}
+}
+class Compra {
+	constructor(){
+		
+	}
+}
+class Vehiculo {
+	constructor(){
+		
+	}
+}
+class Turismo {
+	constructor(){
+		
+	}
+}
+class V4x4 {
+	constructor(){
+		
+	}
+}
+
+class QuintoCar {
+	constructor(){
+		
+	}
+}
