454d7b3c6ecf827d9720e4afcae295c9a0e1a943
Irene Vios <35557223+irevios@users.noreply.github.com>
2019-11-07 13:31:30 +0100

    Visualizacion beta

--- principal.js
+++ principal.js
@@ -1,1 +1,53 @@
 "use strict";
+var opcion; // Guarda qué se está mostrando y según lo que muestra envia una cosa u otra
+
+// Asignar a los elementos un evento (Evitando poner onclick en el html)
+function eventoEn(elemento,tipo,funcion){
+	let boton=document.getElementById(elemento);
+	boton.addEventListener(tipo, funcion, false);
+}
+eventoEn("clickAltaCliente","click",mostrarAltaCliente);
+eventoEn("clickAltaVehiculo","click",mostrarAltaVehiculo);
+eventoEn("clickCompraVehiculo","click",mostrarCompraVehiculo);
+eventoEn("clickVentaVehiculo","click",mostrarVentaVehiculo);
+
+function mostrarAltaCliente () {
+	opcion = "altaCliente";
+	ocultaCosas(["T-altaCliente","F-altaVehiculo","F-comprarVehiculo","F-venderVehiculo"]);
+}
+function mostrarAltaVehiculo () {
+	opcion = "altaVehiculo";
+	ocultaCosas(["F-altaCliente","T-altaVehiculo","F-comprarVehiculo","F-venderVehiculo"]);
+}
+function mostrarCompraVehiculo () {
+	opcion = "compraVehiculo";
+	ocultaCosas(["F-altaCliente","F-altaVehiculo","T-comprarVehiculo","F-venderVehiculo"]);
+}
+function mostrarVentaVehiculo () {
+	opcion = "ventaVehiculo";
+	ocultaCosas(["F-altaCliente","F-altaVehiculo","F-comprarVehiculo","T-venderVehiculo"]);
+}
+
+// Mostrar/Ocultar elementos
+function ocultaCosas(array){
+	formulario.classList.remove("oculta");
+	for (var i = 0; i < array.length; i++) {
+		if(array[i].substring(0,1)=="F"){ //Si no se tiene que mostrar:
+			document.getElementById(array[i].substring(2)).classList.add("oculta"); // Oculta los divs que no se deben mostrar
+			document.getElementById("menu").getElementsByTagName('li')[i].classList.remove("active"); // le quita en el menú el color blanco
+			deshabilitaInputs(array[i].substring(2),true); // todos los inputs dentro del elemento los deshabilita
+		}
+		else if(array[i].substring(0,1)=="T"){ //Si se tiene que mostrar:
+			document.getElementById(array[i].substring(2)).classList.remove("oculta"); // Muestra los divs que no se deben mostrar
+			document.getElementById("menu").getElementsByTagName('li')[i].classList.add("active"); // le añade en el menú el color blanco
+			deshabilitaInputs(array[i].substring(2),false); // todos los inputs dentro del elemento los habilita
+		}
+	}
+}
+
+// Los campos requeridos se deshabilitan dejando solo los que se muestran activos
+function deshabilitaInputs(elemento,NoSi){ 
+	for(let i=0; i < document.getElementById(elemento).getElementsByTagName('input').length;i++){
+		document.getElementById(elemento).getElementsByTagName('input')[i].disabled = NoSi;
+	}
+}
