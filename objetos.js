e4c96e70d5daa48d9759650fdea2259ff117e169
Adolfo Fuentes González <pantocreitor@gmail.com>
2019-11-11 09:09:02 +0100

    Update objetos.js

--- objetos.js
+++ objetos.js
@@ -16,11 +16,11 @@ class Cliente {
     }
 }
 class Venta {
-    constructor(Cliente, Vehiculo, importe, fVenta) {
+    constructor(Cliente, Vehiculo, importe, fechaVenta) { //importe es float
         this.Cliente = Cliente;
         this.Vehiculo = Vehiculo;
         this.importe = importe;
-        this.fVenta = fVenta;
+        this.fechaVenta = fechaVenta;
     }
     toHtmlRow() {
         let row = "<tr>";
@@ -33,19 +33,30 @@ class Venta {
         row += "<td>" + this.Vehiculo.modelo + "</td>";
         row += "<td>" + this.Vehiculo.combustible + "</td>";
         row += "<td>" + this.Venta.importe + "</td>";
-        row += "<td>" + this.Venta.fVenta + "</td>";
+        row += "<td>" + this.Venta.fechaVenta + "</td>";
         row += "</tr>";
     }
 }
 class Compra {
-    constructor(Cliente, Vehiculo, importe, fCompra) {
+    constructor(Cliente, Vehiculo, importe, fechaCompra) {
         this.Cliente = Cliente;
         this.Vehiculo = Vehiculo;
         this.importe = importe;
-        this.fCompra = fCompra;
+        this.fechaCompra = fechaCompra;
     }
     toHtmlRow() {
-
+        let row = "<tr>";
+        row += "<td>" + this.Cliente.nif + "</td>";
+        row += "<td>" + this.Cliente.nombre + "</td>";
+        row += "<td>" + this.Cliente.apellidos + "</td>";
+        row += "<td>" + this.Cliente.telefono + "</td>";
+        row += "<td>" + this.Vehiculo.matricula + "</td>";
+        row += "<td>" + this.Vehiculo.marca + "</td>";
+        row += "<td>" + this.Vehiculo.modelo + "</td>";
+        row += "<td>" + this.Vehiculo.combustible + "</td>";
+        row += "<td>" + this.Compra.importe + "</td>";
+        row += "<td>" + this.Compra.fechaCompra + "</td>";
+        row += "</tr>";
     }
 }
 class Vehiculo {
@@ -65,8 +76,8 @@ class Vehiculo {
     }
 }
 class Turismo extends Vehiculo {
-    super(matricula, marca, modelo, combustible);
-    constructor(abs, descapotable, numPuertas) {
+    constructor(matricula, marca, modelo, combustible, abs, descapotable, numPuertas) {
+        super(matricula, marca, modelo, combustible);
         this.abs = abs;
         this.descapotable = descapotable;
         this.numPuertas = numPuertas;
@@ -84,8 +95,8 @@ class Turismo extends Vehiculo {
     }
 }
 class V4x4 {
-    super(matricula, marca, modelo, combustible);
-    constructor(pendienteMax) {
+    constructor(matricula, marca, modelo, combustible, pendienteMax) {
+        super(matricula, marca, modelo, combustible);
         this.pendienteMax = pendienteMax;
     }
     toHtmlRow() {
@@ -106,5 +117,51 @@ class QuintoCar {
         this.Compras = [];
         this.Vehiculos = [];
     }
+    altaCliente(cliente) {
+        return String;
+    }
 
+    altaVehiculo(vehiculo) {
+        return String
+    }
+
+    buscarCliente(nif) {
+        return Cliente;
+    }
+
+    buscarCompra(Vehiculo) {
+        return Compra;
+    }
+
+    buscarVenta(Vehiculo) {
+        return Venta;
+    }
+
+    Comprar(matricula, nif, importe, fecha) {
+        return String;
+    }
+
+    Vender(matricula, nif, importe, fecha) {
+        return String;
+    }
+
+    listadoALaVenta() {
+        return table;
+    }
+
+    listadoVendidosperiodo(fechaInicio, fechaFin) {
+        return table;
+    }
+
+    listadoComprasperiodo(fechaInicio, fechaFin) {
+        return table;
+    }
+
+    listadoVehiculos() {
+        return table;
+    }
+
+    listadoClientes() {
+        return table;
+    }
 }
