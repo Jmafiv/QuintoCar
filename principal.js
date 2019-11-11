3f47f54bcae540691677ace1ccf590ff10d65a80
Irene Vios <35557223+irevios@users.noreply.github.com>
2019-11-11 10:45:21 +0100

    update

--- principal.js
+++ principal.js
@@ -133,10 +133,10 @@ function clickLVehiculos(){	abreListadoPestaña("Listado vehículos","Vehículos
 function clickLClientes(){ abreListadoPestaña(quintocar.listadoClientes(),"Clientes"); }
 function abreListadoPestaña(listado,titulo){
 	if(!pestañaAbierta){
-		pestañaListados = window.open("",titulo,"");
+		pestañaListados = window.open("");
 		pestañaAbierta = true;
 	}
-	pestañaListados.document.head.innerHTML = '<meta charset="utf-8"><link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">'
+	pestañaListados.document.head.innerHTML = '<meta charset="utf-8"><link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"><title>'+titulo+'</title>';
 	pestañaListados.document.body.innerHTML = listado;
 }
 
