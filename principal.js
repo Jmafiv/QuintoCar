d27036554f42ceaa48b199e73216dee73c93e394
Irene Vios <35557223+irevios@users.noreply.github.com>
2019-11-11 11:26:56 +0100

    Añadido modal para elegir periodo

--- principal.js
+++ principal.js
@@ -127,8 +127,8 @@ eventoEn("clickLVehiculos","click",clickLVehiculos);
 eventoEn("clickLClientes","click",clickLClientes);	
 
 function clickLVehiculoVenta(){ abreListadoPestaña("Listado vehículos en venta","Vehículos en venta"); }
-function clickLVendidos(){ abreListadoPestaña("Listado vehículos vendidos","Vehículos vendidos"); }
-function clickLComprados(){	abreListadoPestaña("Listado vehículos comprados","Vehículos comprados"); }
+//function clickLVendidos(){ abreListadoPestaña("Listado vehículos vendidos","Vehículos vendidos"); }
+//function clickLComprados(){	abreListadoPestaña("Listado vehículos comprados","Vehículos comprados"); }
 function clickLVehiculos(){	abreListadoPestaña("Listado vehículos","Vehículos"); }
 function clickLClientes(){ abreListadoPestaña(quintocar.listadoClientes(),"Clientes"); }
 
