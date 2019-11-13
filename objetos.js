c4ee18ef3d1b56550c73604acc9aafa7aaab9ac9
irevios <irevios@gmail.com>
2019-11-14 00:41:13 +0100

    ordenar fecha

--- objetos.js
+++ objetos.js
@@ -282,10 +282,8 @@ class QuintoCar {
         // Cabecera de la tabla
         let tabla = "<div class='table-responsive'><table class='table'><thead class='thead-dark'><tr><th scope='col'>Matricula</th><th scope='col'>Marca</th><th scope='col'>Modelo</th><th scope='col'>Combustible</th><th scope='col'>ABS</th><th scope='col'>Descapotable</th><th scope='col'>Nº Puertas</th><th scope='col'>Pendiente Máxima</th><th scope='col'>Fecha Compra</th><th scope='col'>Fecha Venta</th><th scope='col'>Importe Compra</th><th scope='col'>Importe Venta</th><th scope='col'>Beneficio</th></tr></thead><tbody>";
         // Ordenación ascendente
-        this.ventas.sort(function(venta,otraVenta){
-            if (venta.fechaVenta > otraVenta.fechaVenta) {return 1;}
-            if (venta.fechaVenta < otraVenta.fechaVenta) {return -1;}
-            return 0;
+        this.ventas.sort((venta,otraVenta) => {
+            return venta.fechaVenta - otraVenta.fechaVenta;
         });
         // Línea de cada venta
         this.ventas.forEach( (venta) => {
@@ -302,10 +300,8 @@ class QuintoCar {
         // Cabecera de la tabla
         let tabla = "<div class='table-responsive'><table class='table'><thead class='thead-dark'><tr><th scope='col'>Matricula</th><th scope='col'>Marca</th><th scope='col'>Modelo</th><th scope='col'>Combustible</th><th scope='col'>ABS</th><th scope='col'>Descapotable</th><th scope='col'>Nº Puertas</th><th scope='col'>Pendiente Máxima</th><th scope='col'>Fecha Compra</th><th scope='col'>Importe Compra</th><th scope='col'>NIF</th><th scope='col'>Nombre</th></tr></thead><tbody>";
         // Ordenación descendente 
-        this.compras.sort(function(compra,otraCompra){
-            if (compra.fechacompra < otraCompra.fechaVenta) {return 1;}
-            if (compra.fechaVenta > otraCompra.fechaVenta) {return -1;}
-            return 0;
+        this.compras.sort((compra,otraCompra) =>{
+            return otraCompra.fechacompra - compra.fechacompra;
         });
         // Línea de cada compra
         this.compras.forEach( (compra) => {
